f87c411e65d0994c7304d284e506e6e2ab60444d
Myles Lake <myles52@live.ca>
2022-05-14 16:23:38 -0400

    Pills do damage. Pills rotate.

--- Assets/Scripts/Enemies/Pill.cs
+++ Assets/Scripts/Enemies/Pill.cs
@@ -6,17 +6,22 @@ public class Pill : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3f;
     [SerializeField] float timeAlive = 2f;
+    [SerializeField] float rotateSpeed = 50f;
+
+    Vector3 rotationDirection;
 
     // Start is called before the first frame update
     void Start()
     {
-        WaitAndDestroy();
+        rotationDirection = Random.Range(1, 3) % 2 == 0 ? Vector3.back : Vector3.forward;
+        StartCoroutine(WaitAndDestroy());
     }
 
     // Update is called once per frame
     void Update()
     {
         gameObject.transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        gameObject.transform.Rotate(rotationDirection * rotateSpeed * Time.deltaTime);
     }
 
     IEnumerator WaitAndDestroy()
